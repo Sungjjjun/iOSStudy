85d74797ee1e2e3e6341377fac3e6ce331e6e6d4
Sunjjjun <psg1478795@naver.com>
2023-11-21 17:08:02 +0900

    2-11. <프로젝트> 심플웨더_04

--- HelloiOS/SimpleWeatherViewController.swift
+++ HelloiOS/SimpleWeatherViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class SimpleWeatherViewController: UIViewController {
+    @IBOutlet weak var temp: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    let cities = ["Seoul", "Tokyo", "Seattle", "LA"]
+    
+    let images = ["sun.max.fill", "cloud.fill", "wind", "cloud.sun.rain.fill"]
 
+    func reload() {
+        let randomTemp = Int.random(in: 10..<30)
+        city.text = cities.randomElement()!
+        imageView.image = UIImage(systemName: images.randomElement()!)?.withRenderingMode(.alwaysOriginal)
+        temp.text = "\(randomTemp)°"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        reload()
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        reload()
+    }
+    
     /*
     // MARK: - Navigation
 
