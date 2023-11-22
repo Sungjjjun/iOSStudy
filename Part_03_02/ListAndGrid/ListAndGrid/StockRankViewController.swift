e7502daa3e807a4410f2277ff2faaa93dc2bbc98
Sunjjjun <psg1478795@naver.com>
2023-11-22 17:02:49 +0900

    3-3. <프로젝트> 주식리스트_02

---
+++ Part_03_02/ListAndGrid/ListAndGrid/StockRankViewController.swift
@@ -0,0 +1,19 @@
+//
+//  StockRankViewController.swift
+//  ListAndGrid
+//
+//  Created by 박성준 on 2023/11/22.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
