51f110f41d46612edda0d706fc72e86ed4e37e83
Sunjjjun <psg1478795@naver.com>
2023-11-23 16:38:20 +0900

    3-4. <프로젝트> 주식리스트_03

---
+++ Project/ListAndGrid/ListAndGrid/StockRankCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StockRankCollectionViewCell.swift
+//  ListAndGrid
+//
+//  Created by 박성준 on 2023/11/23.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    
+}
--- Project/ListAndGrid/ListAndGrid/StockRankViewController.swift
+++ Project/ListAndGrid/ListAndGrid/StockRankViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class StockRankViewController: UIViewController {
 
+    let stockList: [StockModel] = StockModel.list
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
+    //Data - 어떤 데이터, Presentation - 셀을 어떻게 표현, Layout - 셀들의 레이아웃을 어떻게 표현
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+}
 
+extension StockRankViewController: UICollectionViewDataSource {
+    // 데이터가 얼만큼 들어와??
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return stockList.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StockRankCollectionViewCell", for: indexPath)
+        return cell
         
     }
+}
+
+extension StockRankViewController: UICollectionViewDelegateFlowLayout {
+    
 }
