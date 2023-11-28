f15018ca132d32d60165e5f97ad0e5c8a10a4fe3
Sunjjjun <psg1478795@naver.com>
2023-11-28 14:12:29 +0900

    4-3. <프로젝트> 인스타 서치뷰_02

--- Project/InstaSearchView/InstaSearchView/InstaSearchViewController.swift
+++ Project/InstaSearchView/InstaSearchView/InstaSearchViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class InstaSearchViewController: UIViewController {
-
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
+        }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 }
 
+extension InstaSearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InstaSearchCollectionViewCell", for: indexPath) as? InstaSearchCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let imageName = "animal\(indexPath.item + 1)"
+        cell.configure(imageName)
+        return cell
+    }
+}
+
+extension InstaSearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let interSpace: CGFloat = 1
+        let width = (collectionView.bounds.width - 2 * interSpace) / 3
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
+}
