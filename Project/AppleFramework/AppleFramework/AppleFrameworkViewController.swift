aff1ded39c2bc44b2305a8c6629af21bbe3da633
Sunjjjun <psg1478795@naver.com>
2023-11-30 00:49:43 +0900

    6-2. <프로젝트> 애플 프레임워크(CompositionLayout)_01

--- Project/AppleFramework/AppleFramework/AppleFrameworkViewController.swift
+++ Project/AppleFramework/AppleFramework/AppleFrameworkViewController.swift
@@ -8,62 +8,32 @@
 import UIKit
 
 class AppleFrameworkViewController: UIViewController {
-
     @IBOutlet weak var collectionView: UICollectionView!
     
+    enum Section {
+        case main
+    }
+    
+    // AppleFramework -> Item (redirecting)
+    typealias Item = AppleFramework
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     let list: [AppleFramework] = AppleFramework.list
     
     // Data, Presentation, Layout
+    // diffable data source
+    // - presentation
+    // snapshot
+    // - data
+    // compositional layout
+    // - layout
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.dataSource = self
         collectionView.delegate = self
-        
         navigationController?.navigationBar.topItem?.title = "🥸 I'm from Codes"
-        
-        // CollectionView의 Estimated Size == none 적용 코드로
-        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            layout.estimatedItemSize = .zero
-        }
-        
-        // 패딩 적용
-        collectionView.contentInset = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
     }
 }
 
-extension AppleFrameworkViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return list.count
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AppleFrameworkCollectionViewCell", for: indexPath) as? AppleFrameworkCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        cell.configure(list[indexPath.item])
-        return cell
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-}
-
-extension AppleFrameworkViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let interItemSpacing: CGFloat = 10
-        let padding: CGFloat = 16
-        // 2줄
-        // let width = (collectionView.bounds.width - interItemSpacing - 2 * padding) / 2
-        // 3줄
-         let width = (collectionView.bounds.width - 2 * interItemSpacing - 2 * padding) / 3
-        // 4줄
-//        let width = (collectionView.bounds.width - 3 * interItemSpacing - 2 * padding) / 4
-        return CGSize(width: width, height: width * 1.5)
-    }
-}
 
 // 클릭된 앱이 무엇인지
 extension AppleFrameworkViewController: UICollectionViewDelegate {
