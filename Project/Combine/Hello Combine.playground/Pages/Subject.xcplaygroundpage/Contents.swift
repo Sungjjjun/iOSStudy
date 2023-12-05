cfae10a51d783936bb0d08d121c460489c61a6af
Sunjjjun <psg1478795@naver.com>
2023-12-05 18:07:57 +0900

    Part4-2. <실습> Combine_02

--- Project/Combine/Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Subject.xcplaygroundpage/Contents.swift
@@ -2,13 +2,25 @@ import Foundation
 import Combine
 
 // PassthroughSubject
-
-
-
+let relay = PassthroughSubject<String, Never>()
+let subscription1 = relay.sink { value in
+    print("Subscription1 received value: \(value)")
+}
+relay.send("Hello")
+relay.send("World!!!!")
 
 // CurrentValueSubject
+let variable = CurrentValueSubject<String, Never>("Default") // CurrentValueSubject는 초기값이 필요
+variable.send("Initial Data")
+let subscription2 = variable.sink { value in
+    print("Subscription2 received value: \(value)")
+}
+variable.send("Hello")
+variable.send("World!!!")
+variable.value
 
-
-
-
-
+// relay.send("Here")
+// relay.send("We")
+// relay.send("Go")
+let publisher = ["Here", "We", "Go"].publisher
+publisher.subscribe(relay)
