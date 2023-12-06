4c8d41395845fd19f4e857c670709923d089c8e7
Sunjjjun <psg1478795@naver.com>
2023-12-06 18:17:07 +0900

    Part4-2. <실습> Combine_06

--- Project/Combine/Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -3,8 +3,16 @@
 import Foundation
 import Combine
 
-
-
-
-
+let arrayPublisher = [1, 2, 3, 4, 5].publisher
+let queue = DispatchQueue(label: "MyQueue")
+let subscription1 = arrayPublisher
+    .subscribe(on: queue)
+    .map({ value -> Int in
+        print("Tranform: \(value), Thread: \(Thread.current)")
+        return value
+    })
+    .receive(on: DispatchQueue.main)
+    .sink { value in
+        print("Subscriber received: \(value), Thread: \(Thread.current)")
+}
 //: [Next](@next)
