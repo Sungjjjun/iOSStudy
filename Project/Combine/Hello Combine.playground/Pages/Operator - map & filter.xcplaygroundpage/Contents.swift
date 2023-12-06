4c6cd1acf70e8219219add724225e96d1962cf3e
Sunjjjun <psg1478795@naver.com>
2023-12-06 19:37:06 +0900

    Part4-2. <실습> Combine_07

--- Project/Combine/Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Operator - map & filter.xcplaygroundpage/Contents.swift
@@ -4,9 +4,23 @@ import Foundation
 import Combine
 
 // Transform - Map
-
+let numPublisher = PassthroughSubject<Int, Never>()
+let subscription1 = numPublisher
+    .map{ $0 * 2}
+    .sink { value in
+    print("Subscription Received: \(value)")
+}
+numPublisher.send(10)
+subscription1.cancel()
 
 // Filter
-
-
+let stringPublisher = PassthroughSubject<String, Never>()
+let subscription2 = stringPublisher
+    .filter { string in return string.contains("a") }
+    .sink { value in
+        print("Subscription2 Received: \(value)")
+    }
+stringPublisher.send("Ananymous")
+stringPublisher.send("Send")
+stringPublisher.send(completion: .finished)
 //: [Next](@next)
