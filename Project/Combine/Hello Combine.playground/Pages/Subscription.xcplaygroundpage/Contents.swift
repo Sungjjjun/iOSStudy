aa1b4ab2789fccfe494fd8bf509f4564967f093d
Sunjjjun <psg1478795@naver.com>
2023-12-06 15:27:22 +0900

    Part4-2. <실습> Combine_03

--- Project/Combine/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,22 @@
 import Foundation
 import Combine
 
-
-
-
+let subject = PassthroughSubject<String, Never>()
 // The print() operator prints you all lifecycle events
+let subscription = subject
+    .print("[Debug]")
+    .sink { value in
+    print("Subscriber received value: \(value)")
+}
+subject.send("Hello")
+subject.send("Everyone")
 
+// Publisher가 관계 끝냄
+//subject.send(completion: .finished)
+
+// Subscriber가 관계 끝냄
+subscription.cancel()
+
+subject.send("Hello, Are you there??")
 
 //: [Next](@next)
