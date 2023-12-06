f0572d2d7b85b5ba9371f7c0c5b18f2b9bc772ed
Sunjjjun <psg1478795@naver.com>
2023-12-06 16:32:28 +0900

    Part4-2. <실습> Combine_05

--- Project/Combine/Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Foundation and Combine.xcplaygroundpage/Contents.swift
@@ -4,25 +4,45 @@ import Foundation
 import Combine
 import UIKit
 
-
-
 // URLSessionTask Publisher and JSON Decoding Operator
-
-
-
+struct SomeDecodable: Decodable {}
+URLSession.shared.dataTaskPublisher(for: URL(string: "https://www.google.com")!)
+    .map { (data, response) in
+    return data
+    }
+    .decode(type: SomeDecodable.self, decoder: JSONDecoder())
 
 // Notifications
-
-
-
-
+let center = NotificationCenter.default
+let noti = Notification.Name("MyNoti")
+let notiPublisher = center.publisher(for: noti, object: nil)
+let subscriptionNoti = notiPublisher.sink { _ in
+    print("Notification Received")
+}
+center.post(name: noti, object: nil)
+subscriptionNoti.cancel()
 
 // KeyPath binding to NSObject instances
-
-
+let ageLabel = UILabel()
+print(ageLabel.text)
+Just(28)
+    .map { age in
+        return "Age is \(age)"
+    }
+    .assign(to: \.text, on: ageLabel)
+print(ageLabel.text!)
 
 // Timer
 // autoconnect 를 이용하면 subscribe 되면 바로 시작함
+let timerPublisher = Timer
+    .publish(every: 1, on: .main, in: .common)
+    .autoconnect()
 
+let subscriptionTimer = timerPublisher.sink { time in
+    print("Time is \(time)")
+}
 
+DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+    subscriptionTimer.cancel()
+}
 //: [Next](@next)
