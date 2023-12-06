d11aceb2d30a97334d8e7bfcabe44a91c77629aa
Sunjjjun <psg1478795@naver.com>
2023-12-06 15:38:28 +0900

    Part4-2. <실습> Combine_04

--- Project/Combine/Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,7 +4,22 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name: String = "James"
+    var age: Int = 20
+}
 
+final class Label {
+    var text: String = ""
+}
 
+let label = Label()
+let vm = SomeViewModel()
 
+print(label.text)
+vm.$name.assign(to: \.text, on: label)
+print(label.text)
+
+vm.name = "Nick"
+print(label.text)
 //: [Next](@next)
