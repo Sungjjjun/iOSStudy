944d13daa1aa405111de6df59c427a74bd5c0662
Sunjjjun <psg1478795@naver.com>
2023-12-07 01:02:13 +0900

    Part4-2. <실습> Combine_09

--- Project/Combine/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
+++ Project/Combine/Hello Combine.playground/Pages/Operator - removeDup & compactMap.xcplaygroundpage/Contents.swift
@@ -5,19 +5,50 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-// removeDuplicates
+// removeDuplicates -> 중복된 데이터는 제거하고 저장
+let words = "hey hey hey there! Mr Mr ?"
+    .components(separatedBy: " ")
+    .publisher
+words
+    .removeDuplicates()
+    .sink { value in
+        print("Remove Duplicated Data: \(value)")
+    }
+    .store(in: &subscriptions)
 
+// compactMap -> 다양한 자료형의 데이터를 한가지의 데이터 형으로 변환하여 저장(변환이 안되는 것은 nil)
+let strings = ["a", "1.24", "3", "def", "45", "0.23"].publisher
+strings
+    .compactMap { value in
+        return Float(value)
+    }
+    .sink { float in
+        print("Compact Map Float Data: \(float)")
+    }
+    .store(in: &subscriptions)
 
-// compactMap
+// ignoreOutput -> 데이터의 한계 개수가 되지 않았지만, 데이터를 그만 받을 떄
+let numbers = (1...10_000).publisher
+numbers
+    .ignoreOutput()
+    .sink(receiveCompletion: {print("Completed with: \($0)")},
+          receiveValue: {print($0)})
+    .store(in: &subscriptions)
 
+// prefix -> 특정 개수의 데이터만 받고, 끝냄
+let tens = (1...10).publisher
+tens
+    .prefix(2)
+    .sink(receiveCompletion: {print("Completed with: \($0)")},
+          receiveValue: {print($0)})
+    .store(in: &subscriptions)
 
-
-// ignoreOutput
-
-
-
-// prefix
-
-
+// append
+let number = (0...10).publisher
+let otherNumbers = (5...35).publisher
+number
+    .append(otherNumbers)
+    .sink { print("\($0)", terminator: ", ") }
+    .store(in: &subscriptions)
 
 //: [Next](@next)
