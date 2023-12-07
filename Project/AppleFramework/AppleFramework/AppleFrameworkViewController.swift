5662221bf6217500e0538944c1a457ea1f0d8534
Sunjjjun <psg1478795@naver.com>
2023-12-07 14:10:27 +0900

    Part4-2. <프로젝트> 애플 프레임워크(Combine 전환)_01

--- Project/AppleFramework/AppleFramework/AppleFrameworkViewController.swift
+++ Project/AppleFramework/AppleFramework/AppleFrameworkViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class AppleFrameworkViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
@@ -18,7 +19,12 @@ class AppleFrameworkViewController: UIViewController {
     typealias Item = AppleFramework
     
     var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-    let list: [AppleFramework] = AppleFramework.list
+    
+    // Combine
+    var subscriptions = Set<AnyCancellable>()
+    let didSelect = PassthroughSubject<AppleFramework, Never>()
+    let items = CurrentValueSubject<[AppleFramework], Never>(AppleFramework.list)
+    // @Published var list: [AppleFramework] = AppleFramework.list
     
     // Data, Presentation, Layout
     // diffable data source
@@ -29,7 +35,40 @@ class AppleFrameworkViewController: UIViewController {
     // - layout
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
+        bind()
+    }
+    
+    // 핵심 로직 (Combine으로 구성)
+    private func bind() {
+        // Input: 사용자 입력을 받아서, 처리해야할 것
+        // - item 선택 되었을 때
+        didSelect
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] framework in
+                // FrameworkDetailViewController를 띄우기 (Modal)
+                let storyBoard = UIStoryboard(name: "Detail", bundle: nil)
+                let viewController = storyBoard.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
+                viewController.framework = framework
+                
+                // 모달뷰 에니메이션 스타일
+                // viewController.modalTransitionStyle = .flipHorizontal
+                
+                // 풀스크린
+                // viewController.modalPresentationStyle = .fullScreen
+                self.present(viewController, animated: true)
+            }.store(in: &subscriptions)
         
+        // Output: data, state 변경에 따라서, UI 업데이트
+        // - items가 세팅 되었을 때, CollectionView 업데이트
+        items
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] list in
+                self.applySectionItems(list)
+            }.store(in: &subscriptions)
+    }
+    
+    private func configureCollectionView() {
         // Presentation
         dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AppleFrameworkCollectionViewCell", for: indexPath) as? AppleFrameworkCollectionViewCell else {
@@ -39,16 +78,17 @@ class AppleFrameworkViewController: UIViewController {
             return cell
         })
         
-        //Data
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(list, toSection: .main)
-        dataSource.apply(snapshot)
-        
         //Layout
         collectionView.collectionViewLayout = layout()
-        
         collectionView.delegate = self
+    }
+    
+    private func applySectionItems(_ items: [Item], to section: Section = .main) {
+        //Data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([section])
+        snapshot.appendItems(items, toSection: section)
+        dataSource.apply(snapshot)
     }
     
     private func layout() -> UICollectionViewCompositionalLayout{
@@ -74,18 +114,7 @@ class AppleFrameworkViewController: UIViewController {
 // 클릭된 앱이 무엇인지
 extension AppleFrameworkViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let framework = list[indexPath.item]
-        
-        // FrameworkDetailViewController를 띄우기 (Modal)
-        let storyBoard = UIStoryboard(name: "Detail", bundle: nil)
-        let viewController = storyBoard.instantiateViewController(withIdentifier: "FrameworkDetailViewController") as! FrameworkDetailViewController
-        viewController.framework = framework
-        
-//        // 모달뷰 에니메이션 스타일
-//        viewController.modalTransitionStyle = .flipHorizontal
-//
-//        // 풀스크린
-//        viewController.modalPresentationStyle = .fullScreen
-        present(viewController, animated: true)
+        let framework = items.value[indexPath.item]
+        didSelect.send(framework)
     }
 }
