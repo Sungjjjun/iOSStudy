299cd7e21799b8a97412b2b9b1481232d30d65cf
Sunjjjun <psg1478795@naver.com>
2023-12-08 23:40:31 +0900

    Part4-2. <실습> iOS 네트워크(담당 객체 생성)_02

--- Project/Network/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
+++ Project/Network/Network in iOS.playground/Pages/Using Combine.xcplaygroundpage/Contents.swift
@@ -24,12 +24,45 @@ struct GithubProfile: Codable {
     }
 }
 
+// Network 담당 서비스
+final class NetworkService {
+    let session: URLSession
+    
+    init(configuration: URLSessionConfiguration) {
+        session = URLSession(configuration: configuration)
+    }
+    
+    func fetchProfile(userName: String) -> AnyPublisher<GithubProfile, Error> {
+        let url = URL(string: "https://api.github.com/users/\(userName)")!
+        let publisher = session
+            .dataTaskPublisher(for: url)
+        // 서버에서 받은 response 확인
+            .tryMap { result -> Data in
+                // Response Error
+                guard let httpResponse = result.response as? HTTPURLResponse,
+                        (200..<300).contains(httpResponse.statusCode) else {
+                    let response = result.response as? HTTPURLResponse
+                    let statusCode = response?.statusCode ?? -1
+                    throw NetworkError.responseError(statusCode: statusCode)
+                }
+                return result.data
+            }
+        // 받은 데이터 decoding
+            .decode(type: GithubProfile.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+        return publisher
+    }
+}
 
-
-
-
+let networkService = NetworkService(configuration: .default)
+let subscription = networkService
+    .fetchProfile(userName: "Sungjjjun")
+    .receive(on: RunLoop.main)
+    .print()
+    .sink { completion in
+        print(completion)
+    } receiveValue: { profile in
+        print(profile)
+    }
 
 //: [Next](@next)
-
-
-
