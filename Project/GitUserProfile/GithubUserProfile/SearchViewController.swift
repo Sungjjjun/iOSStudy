52ccd734df40e10d8b66747696639d80371827bb
Sunjjjun <psg1478795@naver.com>
2023-12-09 17:10:36 +0900

    Part5-2. <프로젝트> Github 프로필 가져오기_01

--- Project/GitUserProfile/GithubUserProfile/SearchViewController.swift
+++ Project/GitUserProfile/GithubUserProfile/SearchViewController.swift
@@ -3,6 +3,13 @@ import Combine
 import Kingfisher
 
 class UserProfileViewController: UIViewController {
+    // Setup UI
+    // UserProfile
+    // Bind
+    // Search Controller
+    // Network
+    @Published private(set) var user: UserProfile?
+    var subscription = Set<AnyCancellable>()
     
     @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -12,5 +19,50 @@ class UserProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        embededSearchControl()
+        bind()
+    }
+    
+    private func setupUI() {
+        thumbnail.layer.cornerRadius = 80
+    }
+    
+    private func embededSearchControl() {
+        self.navigationItem.title = "Search"
+        
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Sungjjjun"
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        
+        self.navigationItem.searchController = searchController
+    }
+    
+    private func bind() {
+        $user
+            .receive(on: RunLoop.main)
+            .sink { [unowned self] result in
+                self.update(result)
+            }
+            .store(in: &subscription)
+    }
+    
+    private func update(_ user:  UserProfile?) {
+        
+    }
+}
+
+extension UserProfileViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keywords = searchController.searchBar.text ?? ""
+        print("search: \(keywords)")
+    }
+}
+
+extension UserProfileViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("Button Clicked: \(searchBar.text!)")
     }
 }
