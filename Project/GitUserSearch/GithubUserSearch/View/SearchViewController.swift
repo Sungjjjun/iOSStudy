793ef67b87a3928eacd0c34c32c7f664be9ce4e7
Sunjjjun <psg1478795@naver.com>
2023-12-12 16:54:46 +0900

    Part5-2. <프로젝트> Github 사용자 검색하기_01

--- Project/GitUserSearch/GithubUserSearch/View/SearchViewController.swift
+++ Project/GitUserSearch/GithubUserSearch/View/SearchViewController.swift
@@ -2,10 +2,94 @@ import UIKit
 import Combine
 
 class SearchViewController: UIViewController {
-    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    @Published private(set) var users: [SearchResult] = []
+    var subscriptions = Set<AnyCancellable>()
+    
+    enum Section {
+        case main
+    }
+    typealias Item = SearchResult
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        embedSearchController()
+        configureColletionView()
+        bind()
+    }
+    
+    // Search Controller
+    private func embedSearchController() {
+        self.navigationItem.title = "Search Git User"
+        
+        let searchViewController = UISearchController(searchResultsController: nil)
+        searchViewController.hidesNavigationBarDuringPresentation = false
+        searchViewController.searchBar.placeholder = "Input ID What You want to search"
+        searchViewController.searchBar.delegate = self
+        searchViewController.searchResultsUpdater = self
+        
+        self.navigationItem.searchController = searchViewController
+    }
+    
+    // CollectionView 구성
+    private func configureColletionView() {
+        // Presentation -> Diffable Data Source
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {
+                return nil
+            }
+            cell.user.text = item.login
+            return cell
+        })
+
+        // Layout -> Compositional Layout
+        collectionView.collectionViewLayout = layout()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(60))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    // Data Binding
+    private func bind() {
+        // - Data -> View
+        $users
+            .receive(on: RunLoop.main)
+            .sink { users in
+                // 검색된 사용자를 CollectionView Update (Snapshot)
+                var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(users, toSection: .main)
+                self.dataSource.apply(snapshot)
+            }
+            .store(in: &subscriptions)
+        
+        // - 사용자 Interaction 대응 (Search Controller에서 입력된 텍스트 -> Network 요청)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let keyword = searchBar.text, !keyword.isEmpty else { return }
+        print("Button Clicked: \(keyword)")
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let keyword = searchController.searchBar.text ?? ""
+        print("Input Text: \(keyword)")
     }
 }
