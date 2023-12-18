984508effec635b01063c9cf6d9e0310a5b6bfa4
Sunjjjun <psg1478795@naver.com>
2023-12-18 17:04:12 +0900

    Part7-5. <프로젝트> SwiftUI 리스트 구현 (주식 리스트)_01

---
+++ Project/StockRank-SwiftUI/StockRank-SwiftUI/StockRankRow.swift
@@ -0,0 +1,55 @@
+//
+//  StockRankRow.swift
+//  StockRank-SwiftUI
+//
+//  Created by 박성준 on 2023/12/18.
+//
+
+import SwiftUI
+
+struct StockRankRow: View {
+    
+    var stockModel: StockModel
+    
+    var body: some View {
+        HStack {
+            Text("\(stockModel.rank)")
+                .font(.system(size: 15, weight: .bold))
+                .frame(width: 30)
+                .foregroundColor(.blue)
+            Image(stockModel.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            VStack(alignment: .leading, spacing: 5) {
+                Text(stockModel.name)
+                    .font(.system(size: 12))
+                    .foregroundColor(.white)
+                HStack {
+                    Text("\(stockModel.price)원")
+                        .font(.system(size: 12))
+                        .foregroundColor(.gray)
+                    Text("\(stockModel.diff * 100, specifier: "%.2f")%")
+                        .font(.system(size: 12))
+                        .foregroundColor(stockModel.diff >= 0 ? .red : .blue)
+                }
+            }
+            Spacer()
+            Image(systemName: "heart.fill")
+                .resizable()
+                .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 30, height: 30)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .frame(maxWidth: .infinity)
+        .background(.black)
+    }
+}
+
+struct StockRankRow_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankRow(stockModel: StockModel.list[2])
+    }
+}
