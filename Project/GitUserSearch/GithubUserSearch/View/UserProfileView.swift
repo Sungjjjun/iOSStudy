879287998105e577ac43bc7c6bd9fd1a83f515d6
Sunjjjun <psg1478795@naver.com>
2023-12-21 15:44:35 +0900

    Part7-9. <프로젝트> SwiftUI & UIKit 같이 사용하기 (Github 사용자 검색)_02

--- Project/GitUserSearch/GithubUserSearch/View/UserProfileView.swift
+++ Project/GitUserSearch/GithubUserSearch/View/UserProfileView.swift
@@ -6,30 +6,42 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct UserProfileView: View {
     @StateObject var viewModel: UserProfileViewModel
     
     var body: some View {
-        VStack(spacing: 30) {
-            Image(systemName: "person.crop.square.fill")
+        VStack(spacing: 80) {
+            KFImage(viewModel.imageURL)
                 .resizable()
                 .renderingMode(.original)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 160, height: 160)
                 .cornerRadius(80)
-            Text(viewModel.name)
-                .font(.system(size: 30, weight: .bold))
-            Text(viewModel.login)
-                .font(.system(size: 20))
-                .foregroundColor(.gray)
-            Text(viewModel.followers)
-                .font(.system(size: 20))
-                .foregroundColor(.gray)
-            Text(viewModel.following)
-                .font(.system(size: 20))
-                .foregroundColor(.gray)
-        }.padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+            VStack(alignment: .leading, spacing: 30) {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(viewModel.name)
+                        .font(.system(size: 30, weight: .bold))
+                    Text(viewModel.login)
+                        .font(.system(size: 20))
+                        .foregroundColor(.gray)
+                }
+                VStack(alignment: .leading, spacing: 10) {
+                    Text(viewModel.followers)
+                        .font(.system(size: 20))
+                        .foregroundColor(.gray)
+                    Text(viewModel.following)
+                        .font(.system(size: 20))
+                        .foregroundColor(.gray)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(EdgeInsets(top: 0, leading: 40, bottom: 0, trailing: 40))
+        }
+        .onAppear {
+            viewModel.search(keyword: viewModel.loginID)
+        }
     }
 }
 
--- Project/GitUserSearch/GithubUserSearch/ViewModel/UserProfileViewModel.swift
+++ Project/GitUserSearch/GithubUserSearch/ViewModel/UserProfileViewModel.swift
@@ -13,12 +13,14 @@ final class UserProfileViewModel: ObservableObject {
     let network: NetworkService
     
     @Published var selectedUser: UserProfile?
+    var subscriptions = Set<AnyCancellable>()
     
     init(network: NetworkService, loginID: String) {
         self.loginID = loginID
         self.network = network
     }
     
+    var imageURL: URL? { return selectedUser?.avatarUrl }
     var name: String { return selectedUser?.name ?? "n/a" }
     var login: String { return selectedUser?.login ?? "n/a" }
     var followers: String {
@@ -28,5 +30,28 @@ final class UserProfileViewModel: ObservableObject {
     var following: String {
         guard let value = selectedUser?.following else { return "" }
         return "followers: \(value)"
+    }
+    
+    // 주어진 로그인 아이디를 기반으로 UserProfile 검색
+    func search(keyword: String) {
+        let resource = Resource<UserProfile>(
+            base: "https://api.github.com/",
+            path: "users/\(keyword)",
+            params: [:],
+            header: ["Content-Type": "application/json"])
+        
+        network.load(resource)
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    self.selectedUser = nil
+                    print("Error: \(error)")
+                case .finished: break
+                }
+            }, receiveValue: { user in
+                self.selectedUser = user
+            })
+            .store(in: &subscriptions)
     }
 }
