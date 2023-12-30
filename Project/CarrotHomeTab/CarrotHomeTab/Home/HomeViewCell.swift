b2083fda191a4165a765d889247244326e5c7a16
Sunjjjun <psg1478795@naver.com>
2023-12-31 00:51:19 +0900

    Part8-2. <프로젝트> 당근 홈탭 리스트_09

---
+++ Project/CarrotHomeTab/CarrotHomeTab/Home/HomeViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  HomeViewCell.swift
+//  CarrotHomeTab
+//
+//  Created by 박성준 on 2023/12/30.
+//
+
+import UIKit
+import Kingfisher
+
+class HomeViewCell: UICollectionViewCell {
+    
+    @IBOutlet var thumbnailImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var numOfChatsLabel: UILabel!
+    @IBOutlet var numOfLikesLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbnailImage.layer.cornerRadius = 10
+    }
+    
+    func configure(_ item: ItemInfo) {
+        thumbnailImage.kf.setImage(with: URL(string: item.thumbnailURL)!)
+        titleLabel.text = item.title
+        locationLabel.text = item.location
+        priceLabel.text = "\(item.price) 원"
+        numOfChatsLabel.text = "\(item.numOfChats)"
+        numOfLikesLabel.text = "\(item.numOfLikes)"
+    }
+}
--- Project/CarrotHomeTab/CarrotHomeTab/Home/HomeViewController.swift
+++ Project/CarrotHomeTab/CarrotHomeTab/Home/HomeViewController.swift
@@ -9,14 +9,52 @@ import UIKit
 import Combine
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet var collectionView: UICollectionView!
+    
+    enum Section {
+        case main
+    }
+    typealias Item = ItemInfo
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
     let viewModel: HomeViewModel = HomeViewModel(network: NetworkService(configuration: .default))
     var subscriptions = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
         bind()
         viewModel.fetch()
+    }
+    
+    private func configureCollectionView() {
+        // Presentation -> Diffable Data Source
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeViewCell", for: indexPath) as? HomeViewCell else {
+                return nil
+            }
+            cell.configure(item)
+            return cell
+        })
+        
+        // Layout -> Compositional Layout
+        collectionView.collectionViewLayout = layout()
+        collectionView.delegate = self
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(150))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
     }
     
     private func bind() {
@@ -25,6 +63,10 @@ class HomeViewController: UIViewController {
             .receive(on: RunLoop.main)
             .sink { items in
                 print("Update Collection View: \(items)")
+                var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+                snapshot.appendSections([.main])
+                snapshot.appendItems(items, toSection: .main)
+                self.dataSource.apply(snapshot)
             }.store(in: &subscriptions)
         
         // Input(User Interaction)
@@ -41,4 +83,10 @@ class HomeViewController: UIViewController {
 //        let detailViewController = detailStoryBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
 //        navigationController?.pushViewController(detailViewController, animated: true)
 //    }
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected: \(indexPath.item.description)")
+    }
 }
