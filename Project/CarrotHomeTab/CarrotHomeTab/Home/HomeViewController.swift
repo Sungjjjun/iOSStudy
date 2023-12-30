5e505cbb90907b634bd29863c07b563415159c9c
Sunjjjun <psg1478795@naver.com>
2023-12-30 15:47:13 +0900

    Part8-2. <프로젝트> 당근 홈탭 리스트_08

--- Project/CarrotHomeTab/CarrotHomeTab/Home/HomeViewController.swift
+++ Project/CarrotHomeTab/CarrotHomeTab/Home/HomeViewController.swift
@@ -6,16 +6,39 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
 
+    let viewModel: HomeViewModel = HomeViewModel(network: NetworkService(configuration: .default))
+    var subscriptions = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bind()
+        viewModel.fetch()
     }
     
-    @IBAction func ctaButtonTapped(_ sender: Any) {
-        let detailStoryBoard = UIStoryboard(name: "Detail", bundle: nil)
-        let detailViewController = detailStoryBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        navigationController?.pushViewController(detailViewController, animated: true)
+    private func bind() {
+        // Output(Data)
+        viewModel.$items
+            .receive(on: RunLoop.main)
+            .sink { items in
+                print("Update Collection View: \(items)")
+            }.store(in: &subscriptions)
+        
+        // Input(User Interaction)
+        viewModel.itemTapped
+            .sink { item in
+                let detailStoryBoard = UIStoryboard(name: "Detail", bundle: nil)
+                let detailViewController = detailStoryBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+                self.navigationController?.pushViewController(detailViewController, animated: true)
+            }.store(in: &subscriptions)
     }
+    
+//    @IBAction func ctaButtonTapped(_ sender: Any) {
+//        let detailStoryBoard = UIStoryboard(name: "Detail", bundle: nil)
+//        let detailViewController = detailStoryBoard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+//        navigationController?.pushViewController(detailViewController, animated: true)
+//    }
 }
