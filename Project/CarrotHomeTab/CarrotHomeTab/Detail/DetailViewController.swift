9c95148255e6da8fa072f73ce7bd298864204d11
Sunjjjun <psg1478795@naver.com>
2024-01-02 15:36:33 +0900

    Part8-2. <프로젝트> 당근 홈탭 리스트_16

--- Project/CarrotHomeTab/CarrotHomeTab/Detail/DetailViewController.swift
+++ Project/CarrotHomeTab/CarrotHomeTab/Detail/DetailViewController.swift
@@ -24,14 +24,22 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var itemThumbnail: UIImageView!
     @IBOutlet weak var itemInfoTitle: UILabel!
     @IBOutlet weak var itemInfoDescription: UILabel!
+    @IBOutlet weak var itemPriceLabel: UILabel!
     
     var viewModel: DetailViewModel!
     var subscriptions = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigationBar()
+        setupUI()
         bind()
         viewModel.fetch()
+    }
+    
+    private func setupUI() {
+        userThumbnail.layer.masksToBounds = true
+        userThumbnail.layer.cornerRadius = 40
     }
  
     private func bind() {
@@ -46,7 +54,33 @@ class DetailViewController: UIViewController {
                 self.itemThumbnail.kf.setImage(with: URL(string: details.item.thumbnailURL))
                 self.itemInfoTitle.text = details.item.title
                 self.itemInfoDescription.text = details.details.descriptions
+                self.itemPriceLabel.text = "\(self.formatNumber(details.item.price))원"
             }.store(in: &subscriptions)
         
+    }
+    
+    private func formatNumber(_ price: Int) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        let result = formatter.string(from: NSNumber(integerLiteral: price)) ?? ""
+        return result
+    }
+    
+    private func configureNavigationBar() {
+        let moreConfig = CustomBarItemConfigure(
+            image: UIImage(systemName: "ellipsis"),
+            handler: {}
+        )
+        let moreItem = UIBarButtonItem.generate(with: moreConfig, width: 30)
+        
+        let shareConfig = CustomBarItemConfigure(
+            image: UIImage(systemName: "square.and.arrow.up"),
+            handler: {}
+        )
+        let shareItem = UIBarButtonItem.generate(with: shareConfig, width: 30)
+
+        navigationItem.rightBarButtonItems = [moreItem, shareItem]
+        navigationItem.backButtonDisplayMode = .minimal
     }
 }
